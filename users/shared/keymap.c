8986e55c196b7747e5137b59c6cd1bc9b02a2a04
Corintho Assunção <github@corintho.eu>
2026-01-03 11:51:04 +0100

    chore: extract rbg code into dedicated module

--- users/shared/keymap.c
+++ users/shared/keymap.c
@@ -1,11 +1,13 @@
 // Copyright (c) 2025 Corintho Assuncao
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include "modifiers.h"
 #include QMK_KEYBOARD_H
 
 #include "layers.h"
 #include "combos.h"
+#ifdef RGBLIGHT_ENABLE
+#include "rgb/rgb.h"
+#endif
 
 // copied from https://beta.docs.qmk.fm/faqs/faq_debug#which-matrix-position-is-this-keypress
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -40,41 +42,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_ART_NAV] = LAYOUT_ortho_2x4(NAV_1_1, NAV_1_2, NAV_1_3, NAV_1_4, NAV_2_1, NAV_2_2, NAV_2_3, NAV_2_4),
 };
 
-#ifdef RGBLIGHT_ENABLE
-void keyboard_post_init_user(void) {
-    // Initialize RGB to static black
-    rgblight_enable_noeeprom();
-    rgblight_sethsv_noeeprom(HSV_BLACK);
-    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
-}
-
-void keyboard_pre_init_user(void) {
-    // Mark the power led pin as output
-    gpio_set_pin_output(24);
-    // Ensure it is "ON" during startup
-    gpio_write_pin_low(24);
-}
-
-uint8_t mod_state;
-void    housekeeping_task_user(void) {
-    // Adjust RGB color based on modifiers
-    mod_state     = get_mods();
-    uint8_t red   = mod_state & MOD_MASK_ALT ? 255 : 0;
-    uint8_t green = mod_state & MOD_MASK_GUI ? 255 : 0;
-    uint8_t blue  = mod_state & MOD_MASK_CTRL ? 255 : 0;
-    rgblight_setrgb_at(red, green, blue, 0);
-}
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-    switch (get_highest_layer(state)) {
-        // Turn off power led when on NAV layer
-        case _ART_NAV:
-            gpio_write_pin_high(24);
-            break;
-        default: //  for any other layers, or the default layer
-            gpio_write_pin_low(24);
-            break;
-    }
-    return state;
-}
-#endif
